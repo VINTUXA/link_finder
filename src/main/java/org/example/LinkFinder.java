7794a8baa6fae0835a28c2dbffaf7c4f9dff0f65
VINTUXA <dvo.performance@gmail.com>
2024-02-01 23:47:08 +0300

    fix compute logic

--- src/main/java/org/example/LinkFinder.java
+++ src/main/java/org/example/LinkFinder.java
@@ -3,64 +3,57 @@ package org.example;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-
 import java.io.IOException;
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.RecursiveTask;
 
-public class LinkFinder extends RecursiveTask<Map<String, List<String>>> {
+public class LinkFinder extends RecursiveTask<Set<String>> {
     public static Map<String,List<String>> map = new TreeMap<>();
     private String url;
-    private List<LinkFinder> tasks;
-    private int tabLevel;
-
-    public LinkFinder(String url, int tabLevel) {
+    public LinkFinder(String url) {
         this.url = url;
-        this.tasks = new ArrayList<>();
-        this.tabLevel = tabLevel;
+    }
+    public static Map<String, List<String>> getMap() {
+        return map;
     }
 
-
     @Override
-    protected Map<String,List<String>> compute() {
+    protected Set<String>compute() {
+        Set<String> currentUrlSet = new HashSet<>();
         try {
             Thread.sleep(200);
-            Document html = Jsoup.connect(url)
-                    .timeout(1000000)
-                    .header("Accept", "text/javascript")
-                    .get();
+            Document html = getHtml(url);
+            currentUrlSet.add(url);
+
+            //map for output file
             List<String> currentListOfUrls = new ArrayList<>();
             map.put(url, currentListOfUrls);
-            for (Element el : html.select("a")){
-                String currentUrl = el.attr("href").split("\\?")[0];
-                if(checkRequirements(currentUrl)){
-                    LinkFinder task = new LinkFinder(currentUrl, tabLevel + 1);
-                    task.fork();
-                    tasks.add(task);
+
+            // find all child links
+            for (Element el : html.select("a[href]")){
+                String currentUrl = el.attr("href").replaceAll("\\/+$","");
+
+                if (currentUrl.startsWith(url) && !currentUrlSet.contains(currentUrl)){
                     map.get(url).add(currentUrl);
+
+                    // fork task for each child link
+                    LinkFinder task = new LinkFinder(currentUrl);
+                    task.fork();
+                    currentUrlSet.addAll(task.join());
                 }
             }
-            for (LinkFinder task : tasks){
-                task.join();
-            }
-
         } catch (IOException e) {
             throw new RuntimeException(e);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-
-
-        return map;
+        return currentUrlSet;
     }
 
-    private boolean checkRequirements(String url){
-        return isItLinkChildOfRequiredUrl(url)
-                && !map.containsKey(url);
-    }
-
-    private boolean isItLinkChildOfRequiredUrl(String url){
-        return url.contains(this.url);
+    private Document getHtml(String url) throws IOException {
+        return Jsoup.connect(url)
+                .timeout(1000000)
+                .header("Accept", "text/javascript")
+                .get();
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,16 +6,15 @@ import java.util.*;
 import java.util.concurrent.ForkJoinPool;
 
 public class Main {
-    private static final String URL = "https://www.youtube.com";
+    private static final String URL = "https://nopaper.ru";
     private static final String pathToOutFile = "src/main/java/org/example/out/result.txt";
+    static Set<String> usedUrls = new HashSet<>();
     public static void main(String[] args) throws IOException {
 
         int cores = Runtime.getRuntime().availableProcessors();
         ForkJoinPool service = new ForkJoinPool(cores);
-        service.invoke(new LinkFinder(URL, 0));
-        Map<String,List<String>> map = service.invoke(new LinkFinder(URL, 0));
-        printMapToFile(pathToOutFile, map);
-
+        service.invoke(new LinkFinder(URL));
+        printMapToFile(pathToOutFile, LinkFinder.getMap());
     }
 
 
@@ -25,7 +24,10 @@ public class Main {
                 String key = entry.getKey();
                 List<String> values = entry.getValue();
 
-                writer.println(key);
+                if(!usedUrls.contains(key)){
+                    writer.println(key);
+                    usedUrls.add(key);
+                }
                 printNestedUrlsToFile(writer, values, 1, map);
             }
         } catch (Exception e) {
@@ -35,11 +37,13 @@ public class Main {
 
     private static void printNestedUrlsToFile(PrintWriter writer, List<String> urls, int tabLevel, Map<String,List<String>> map) {
         for (String url : urls) {
+            if(usedUrls.contains(url)) continue;
             StringBuilder spaces = new StringBuilder();
             for (int i = 0; i < tabLevel; i++) {
                 spaces.append("    ");
             }
             writer.println(spaces.toString() + url);
+            usedUrls.add(url);
             if (map.containsKey(url)) {
                 printNestedUrlsToFile(writer, map.get(url), tabLevel + 1, map);
             }
